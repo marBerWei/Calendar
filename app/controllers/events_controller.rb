7a92ce6dbd8946f587a0e43b6ec52c79590d1ba9
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-03-06 15:39:13 -0500

    can switch between months

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,37 +2,32 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    @start_date = params.fetch([:start_date], Date.today).to_date
     @weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-	@date_range = (@start_date.beginning_of_month.beginning_of_week..@start_date.end_of_month.end_of_week).to_a
+    # byebug
   end
 
 
-  def show
-    @event = Event.find(params[:id])
+  # def show
+  #   @event = Event.find(params[:id])
+  # end
+  def edit
+  	@event = Event.find(params[:id])
   end
-
 
   def new
     @event = Event.new
   end
 
-  def edit
-  	@event = Event.find(params[:id])
-  end
-
   def create
     @event = Event.new(event_params)
-	# / check for existing events
-	# / maybe there is a way to do that with validation
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+	# check for existing events
+	# maybe there is a way to do that with validation
+	if @event.valid?
+      	@event.save
+      	flash[:notice] = 'Event was successfully created.'
+      	redirect_to events_path
+    else
+        redirect_to new_event_path
     end
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
+
+
 class Event < ApplicationRecord
+	 validates :name, :uniqueness => true, :presence => true
+	 validates :time_start, :uniqueness => true, :presence => true
+	 validates :time_end, :uniqueness => true, :presence => true
 end
+
