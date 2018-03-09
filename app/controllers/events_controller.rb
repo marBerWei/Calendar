c2ce6f186e89ad5e2dd42e04681ff021c9e95079
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-03-09 12:03:23 -0500

    datetime params accepting one object

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,14 +7,19 @@ class EventsController < ApplicationController
   end
 
 
-  # def show
-  #   @event = Event.find(params[:id])
-  # end
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
     @event = Event.new
   end
 
   def create
+  	
+ 	# ev_params = params[:event]
+	# @time_start = Time.zone.local(*ev_params.select {|k,v| k.to_s.index('time_start(') == 0 }.sort.map {|p| p[1].to_i})
+	# @time_end = Time.zone.local(*ev_params.select {|k,v| k.to_s.index('time_end(') == 0 }.sort.map {|p| p[1].to_i})
     @event = Event.new(event_params)
 	if @event.valid?
       	@event.save
@@ -35,7 +40,7 @@ class EventsController < ApplicationController
   	@event = Event.find(params[:id])
   	@event.update(event_params)
   	flash[:notice] = 'Event was successfully updated.'
-  	redirect_to events_path
+  	redirect_to event_path(@event)
   end
 
   def destroy
@@ -48,6 +53,6 @@ class EventsController < ApplicationController
   private
 
     def event_params
-      params.require(:event).permit(:name, :start_time, :end_time)
+      params.require(:event).permit(:name, :time_start, :time_end)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 
 
 class Event < ApplicationRecord
+	self.columns_hash['available_at'] = OpenStruct.new(type: :datetime, class: DateTime)
 	 validates :name, :uniqueness => true, :presence => true
 	 validates :time_start, :uniqueness => true, :presence => true
 	 validates :time_end, :uniqueness => true, :presence => true
